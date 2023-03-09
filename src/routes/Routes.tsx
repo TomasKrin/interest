503cad1d1dfea744ea369b0662a999311aadbf74
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-09 20:45:59 +0200

    created Routes component

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes as RoutesWrapper } from "react-router-dom";
+
+import { routes } from "./consts";
+
 const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <RoutesWrapper>
+      {routes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+    </RoutesWrapper>
+  );
 };
 
 export default Routes;
