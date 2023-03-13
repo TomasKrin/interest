9c544bd59e13ece37aa668eaa1be2818a23f1454
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-13 20:14:59 +0200

    fixed routes with authentification check

--- src/routes/consts.ts
+++ src/routes/consts.ts
@@ -1,12 +1,16 @@
+import { AuthLayoutRoutes, MainLayoutRoutes } from "./routeTypes";
+
+import AuthLayout from "../layouts/AuthLayout";
 import Examples from "../pages/Examples/Examples";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 import MainLayout from "../layouts/MainLayout";
-import { MainLayoutRoutes } from "./routeTypes";
 import Pin from "../pages/Pin/Pin";
 
 export const HOME_PATH = "/";
 export const PIN_PATH = "/:id";
 export const EXAMPLES_PATH = "/examples";
+export const LOGIN_PATH = "/";
 
 export const mainLayoutRoutes: MainLayoutRoutes = {
   Layout: MainLayout,
@@ -15,4 +19,9 @@ export const mainLayoutRoutes: MainLayoutRoutes = {
     { path: PIN_PATH, Component: Pin },
     { path: EXAMPLES_PATH, Component: Examples },
   ],
+};
+
+export const authLayoutRoutes: AuthLayoutRoutes = {
+  Layout: AuthLayout,
+  routes: [{ path: LOGIN_PATH, Component: Login }],
 };
