2c9d146949532346f025045694f5a7d542ed1c62
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-20 20:20:02 +0200

    added react-redux for search logic

--- src/components/SearchBar/ImageSearchBar.tsx
+++ src/components/SearchBar/ImageSearchBar.tsx
@@ -1,7 +1,18 @@
+import { useAppDispatch, useAppSelector } from "../../hooks/store";
+
 import SearchBar from "./SearchBar";
+import { debounce } from "lodash";
+import { onChange } from "../../store/searchSlice";
 
 const ImageSearchBar = () => {
-  return <SearchBar value="" setValue={() => null} />;
+  const value = useAppSelector((state) => state.search.value);
+  const dispatch = useAppDispatch();
+
+  const handleChangeSearch = (value: string) => {
+    dispatch(onChange(value));
+  };
+
+  return <SearchBar value={value} setValue={handleChangeSearch} />;
 };
 
 export default ImageSearchBar;
