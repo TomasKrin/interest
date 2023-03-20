e4ee677540b5d209aad3ca4400ce353d7db9b7d3
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-20 18:38:44 +0200

    extract search bar to separate component

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,55 +1,9 @@
-import { alpha, styled } from "@mui/material/styles";
-
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
+import ImageSearchBar from "../SearchBar/ImageSearchBar";
 import Toolbar from "@mui/material/Toolbar";
 import TopBarMenu from "./TopBarMenu";
 import Typography from "@mui/material/Typography";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  marginRight: theme.spacing(1),
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-    marginRight: theme.spacing(2),
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "22ch",
-      "&:focus": {
-        width: "30ch",
-      },
-    },
-  },
-}));
 
 const TopBar = () => {
   return (
@@ -64,12 +18,7 @@ const TopBar = () => {
           >
             Interest
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
-          </Search>
+          <ImageSearchBar />
           <TopBarMenu />
         </Toolbar>
       </AppBar>
